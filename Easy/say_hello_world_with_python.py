ac61f8e04a04a6ccd61ac67ec2fe50c0ae7ba463
Prathmesh Shelke <prathamesh2526@gmail.com>
2020-06-25 14:22:38 -0400

    Say "Hello, World!" With Python

---
+++ Easy/say_hello_world_with_python.py
@@ -0,0 +1,10 @@
+Say "Hello, World!" With Python
+
+https://www.hackerrank.com/challenges/py-hello-world/problem
+
+
+The above code will print Hello, World! on your screen. Try it yourself in the editor below!
+
+Solution:
+
+print('Hello, World!')
