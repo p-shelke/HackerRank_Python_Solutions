a15fb4f9fc64462ee109a5590ff631a0b5bd87d4
Prathmesh Shelke <prathamesh2526@gmail.com>
2020-06-26 12:00:34 -0400

    Python: Division

--- Easy/python_division.sql
+++ Easy/python_division.sql
@@ -5,6 +5,7 @@ https://www.hackerrank.com/challenges/python-division/problem
 Read two integers and print two lines. The first line should contain integer division,  a//b . The second line should contain float division,  a/b.
 You don't need to perform any rounding or formatting operations.
 
+
 Solution:
 
 a = int(input())
